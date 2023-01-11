3ed5933b2f0967428aa7d9eb55b1e7ba466aebcf
Sviatlana <goldsviatlana@gmail.com>
2023-01-11 17:12:15 +0300

    Docker, tests

--- src/api/v1/theater/tests/test_views.py
+++ src/api/v1/theater/tests/test_views.py
@@ -1,3 +1,4 @@
+from django.test.utils import override_settings
 from rest_framework.test import APITestCase
 
 from core.theater.tests import factories
@@ -9,7 +10,7 @@ class ReserveTicketTestCase(APITestCase):
         self.performance = factories.PerformanceFactory()
         self.ticket = factories.TicketFactory()
 
-    def test_create_reserved_ticket(self):
+    def test_create_reserved_ticket__success(self):
         self.client.force_authenticate(user=self.user)
         response = self.client.post(
             "/api/v1/theater/reserved-tickets/",
@@ -22,3 +23,21 @@ class ReserveTicketTestCase(APITestCase):
         self.assertEqual(response_data["performance"], self.performance.id)
         self.assertEqual(response_data["ticket"], self.ticket.id)
         self.assertEqual(response_data["reserved_by"], self.user.id)
+
+    @override_settings(MAX_TICKET_RESERVATIONS_PER_PERSON=0)
+    def test_create_reserved_ticket__failed(self):
+        self.reserved_ticket = factories.ReservedTicketFactory(
+            ticket=self.ticket, performance=self.performance, reserved_by=self.user
+        )
+        self.client.force_authenticate(user=self.user)
+        response = self.client.post(
+            "/api/v1/theater/reserved-tickets/",
+            data={"performance": self.performance.id, "ticket": self.ticket.id},
+            format="json",
+        )
+
+        response_data = response.json()
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(
+            response_data, {"non_field_errors": ["The fields performance, ticket must make a unique set."]}
+        )
--- src/main/settings.py
+++ src/main/settings.py
@@ -10,14 +10,20 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 import os
+import sys
 from pathlib import Path
 
 from dotenv import load_dotenv
 
-load_dotenv()
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+try:
+    TESTING = "test" == sys.argv[1]
+except IndexError:
+    TESTING = False
+
+load_dotenv(os.path.join(BASE_DIR.parent, ".test.env" if TESTING else ".env"))
 
 
 # Quick-start development settings - unsuitable for production
@@ -29,7 +35,7 @@ SECRET_KEY = "django-insecure-@-i9yuu^$jp1a%t-&j$$!j0c8hp^y(ub-f9wu70sm@o)i_h&do
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["0.0.0.0", "localhost"]
 
 
 # Application definition
@@ -82,11 +88,14 @@ WSGI_APPLICATION = "main.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ.get("POSTGRES_DB"),
+        "USER": os.environ.get("POSTGRES_USER"),
+        "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
+        "HOST": os.environ.get("POSTGRES_HOST"),
+        "PORT": os.environ.get("POSTGRES_PORT"),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
